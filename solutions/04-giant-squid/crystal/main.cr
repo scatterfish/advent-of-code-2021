ba1b57fd4c5c417d81f531ce26130d6cbd582a66
Amy <amy@fluff.tech>
2021-12-04 18:35:32 -0500

    day 4 Crystal solution

---
+++ solutions/04-giant-squid/crystal/main.cr
@@ -0,0 +1,49 @@
+numbers = File.read("input.txt").split("\n\n").map(&.strip.split(/[,\s]+/).map(&.to_i))
+called = numbers.shift
+boards = numbers.map {|grid_numbers| Board.new(grid_numbers)}
+
+class Board
+	
+	@grid : Array(Array(Int32))
+	
+	def initialize(grid_numbers : Array(Int32))
+		@grid = grid_numbers.in_groups_of(5, 0)
+		@called = Array(Int32).new
+	end
+	
+	def add_called(number)
+		@called << number
+	end
+	
+	def won?
+		return {@grid, @grid.transpose}.any? { |grid|
+			grid.any? { |row|
+				row.all? {|number| @called.includes?(number)}
+			}
+		}
+	end
+	
+	def score
+		uncalled_sum = @grid.flatten.reject {|number| @called.includes?(number)}.sum
+		return uncalled_sum * @called.last
+	end
+	
+end
+
+first_board = nil
+last_board = nil
+
+until boards.all?(&.won?)
+	number = called.shift
+	boards.dup.each do |board|
+		board.add_called(number)
+		if board.won?
+			first_board = board if first_board.nil?
+			boards.delete(board)
+		end
+		last_board = boards.first if boards.one?
+	end
+end
+
+puts "Part 1 answer: #{first_board.not_nil!.score}"
+puts "Part 2 answer: #{last_board.not_nil!.score}"
