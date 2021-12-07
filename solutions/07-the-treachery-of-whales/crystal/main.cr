9609ed80dc1a7a61110501c92ffe9904140ab413
Amy <amy@fluff.tech>
2021-12-07 03:06:43 -0500

    day 7 Crystal improvement

--- solutions/07-the-treachery-of-whales/crystal/main.cr
+++ solutions/07-the-treachery-of-whales/crystal/main.cr
@@ -1,10 +1,4 @@
-crabs = File.read("input.txt").strip.split(",").map(&.to_i)
+crabs = File.read("input.txt").strip.split(",").map(&.to_i).sort
 
-costs_1, costs_2 = (crabs.min..crabs.max).map { |pos|
-	crabs.map { |crab|
-		[(crab - pos).abs, (1..(crab - pos).abs).sum]
-	}.transpose.map(&.sum)
-}.transpose
-
-puts "Part 1 answer: #{costs_1.min}"
-puts "Part 2 answer: #{costs_2.min}"
+puts "Part 1 answer: #{crabs.sum {|crab| (crab - crabs[crabs.size // 2]).abs}}"
+puts "Part 2 answer: #{crabs.sum {|crab| (1..(crab - crabs.sum // crabs.size).abs).sum}}"
